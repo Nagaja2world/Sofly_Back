8d84f767f64357d1e4ab19f502749fc83f2f2763
sehyeon <tpgusdla5789@gmail.com>
2026-03-22 23:41:22 +0900

    feat: Schedule service단 초기 구현 및 WorkspaceRepository 생성

---
+++ services/travel-core-service/src/main/java/com/sofly/core/domain/workspace/repository/WorkspaceRepository.java
@@ -0,0 +1,8 @@
+package com.sofly.core.domain.workspace.repository;
+
+import com.sofly.core.domain.workspace.entity.Workspace;
+import org.springframework.data.repository.CrudRepository;
+
+public interface WorkspaceRepository extends CrudRepository<Workspace, Long> {
+
+}
