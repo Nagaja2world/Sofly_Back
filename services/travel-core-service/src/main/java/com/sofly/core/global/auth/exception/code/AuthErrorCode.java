128eb94d2f51425401363fd0879aba095472f237
Park Sang Min <sm0104224@gmail.com>
2026-04-01 00:12:49 +0900

    feat: response and AuthErroCode Error Code config

--- services/travel-core-service/src/main/java/com/sofly/core/global/auth/exception/code/AuthErrorCode.java
+++ services/travel-core-service/src/main/java/com/sofly/core/global/auth/exception/code/AuthErrorCode.java
@@ -11,7 +11,6 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum AuthErrorCode implements BaseErrorCode {
 
-
     INCORRECT_PASSWORD(HttpStatus.UNAUTHORIZED,"INCORRECT_PASSWORD", "비밀번호가 틀렸습니다"),
     TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "TOKEN_EXPIRED", "토큰이 만료되었습니다."),
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "INVALID_TOKEN", "유효하지 않은 토큰입니다."),
@@ -21,8 +20,7 @@ public enum AuthErrorCode implements BaseErrorCode {
     // 회원가입 시 중복된 아이디가 있을 경우
     DUPLICATE_LOGIN_ID(HttpStatus.CONFLICT, "DUPLICATE_LOGIN_ID", "이미 존재하는 아이디입니다."),
     EMPTY_AUTHENTICATION(HttpStatus.UNAUTHORIZED, "EMPTY_AUTHENTICATION", "인증 정보가 존재하지 않습니다."),
-    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER_NOT_FOUND", "해당 사용자를 찾지 못했습니다.")
-    ;
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER_NOT_FOUND", "해당 사용자를 찾지 못했습니다.");
 
     private final HttpStatus status;
     private final String code;
--- services/travel-core-service/src/main/java/com/sofly/core/global/response/code/ErrorStatus.java
+++ services/travel-core-service/src/main/java/com/sofly/core/global/response/code/ErrorStatus.java
@@ -12,13 +12,8 @@ public enum ErrorStatus implements BaseErrorCode {
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON_001", "서버 오류가 발생했습니다."),
 
     INVALID_INPUT(HttpStatus.BAD_REQUEST, "COMMON_002", "잘못된 요청입니다."),
-
     // 인증
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "AUTH_001", "인증이 필요합니다."),
-
-    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "AUTH_002", "유효하지 않은 토큰입니다."),
-    
-    TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "AUTH_003", "만료된 토큰입니다."),
 
     REFRESH_TOKEN_NOT_FOUND(HttpStatus.UNAUTHORIZED, "AUTH_004", "Refresh Token을 찾을 수 없습니다."),
 
@@ -26,9 +21,6 @@ public enum ErrorStatus implements BaseErrorCode {
 
     // 400 Bad Request: 클라이언트의 요청이 잘못됨
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "COMMON400", "잘못된 요청입니다."),
-
-    // 401 Unauthorized: 인증이 필요한 서비스에서 인증이 안 됨
-    // UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "COMMON401", "인증이 필요합니다."),
 
     // 403 Forbidden: 인증은 됐으나 해당 리소스에 권한이 없음
     FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "제한된 접근입니다."),
@@ -38,9 +30,6 @@ public enum ErrorStatus implements BaseErrorCode {
 
     // 409 Conflict: 서버의 현재 상태와 요청이 충돌함
     CONFLICT(HttpStatus.CONFLICT, "COMMON409", "데이터 충돌이 발생했습니다.");
-
-    // 500 Internal Server Error: 서버 내부 에러
-    // INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON500", "예기치 않은 서버 에러가 발생했습니다.");
 
     private final HttpStatus status;
     private final String code;
