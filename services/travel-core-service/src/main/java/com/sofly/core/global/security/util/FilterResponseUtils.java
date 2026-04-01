5e38366c5b0bfda7b91d6f01279831c5b8d07e5b
Park Sang Min <sm0104224@gmail.com>
2026-04-01 19:02:31 +0900

    chore: unnessary comment delete

--- services/travel-core-service/src/main/java/com/sofly/core/global/security/util/FilterResponseUtils.java
+++ services/travel-core-service/src/main/java/com/sofly/core/global/security/util/FilterResponseUtils.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sofly.core.global.response.ApiResponse;  // sofly 패키지에 맞게
+import com.sofly.core.global.response.ApiResponse;
 import com.sofly.core.global.response.code.BaseErrorCode;
 
 import jakarta.servlet.http.HttpServletResponse;
@@ -20,7 +20,6 @@ public class FilterResponseUtils {
     public void sendUnauthorized(HttpServletResponse response, BaseErrorCode errorCode) throws IOException {
         response.setContentType("application/json;charset=UTF-8");
         response.setStatus(errorCode.getStatus().value());
-        // sofly ApiResponse 구조에 맞게 수정 필요
         ApiResponse<Object> errorResponse = ApiResponse.fail(errorCode);
         String json = objectMapper.writeValueAsString(errorResponse);
         response.getWriter().write(json);
