5640ecd7c90b0c6e8d068bbc0e133d887bb2d6e3
Park Sang Min <sm0104224@gmail.com>
2026-04-03 00:54:16 +0900

    fix: UserErroCode PROFILE_UPDATE_FAILED delete

--- services/travel-core-service/src/main/java/com/sofly/core/domain/user/code/UserErrorCode.java
+++ services/travel-core-service/src/main/java/com/sofly/core/domain/user/code/UserErrorCode.java
@@ -11,9 +11,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum UserErrorCode implements BaseErrorCode {
 
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER_001", "사용자를 찾을 수 없습니다."),
-    PROFILE_UPDATE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "USER_002", "프로필 업데이트에 실패했습니다.");
-
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER_001", "사용자를 찾을 수 없습니다.");
 
     private final HttpStatus status;
     private final String code;
