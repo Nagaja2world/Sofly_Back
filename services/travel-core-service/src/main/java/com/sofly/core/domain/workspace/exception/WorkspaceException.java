a1c87c8770d8410c932cc4c27d112fbe63f4562e
Park Sang Min <sm0104224@gmail.com>
2026-04-05 23:54:20 +0900

    refactor(workspace): WorkspaceErrorCode → BaseErrorCode로 인터페이스 통일

--- services/travel-core-service/src/main/java/com/sofly/core/domain/workspace/exception/WorkspaceException.java
+++ services/travel-core-service/src/main/java/com/sofly/core/domain/workspace/exception/WorkspaceException.java
@@ -1,15 +1,21 @@
 package com.sofly.core.domain.workspace.exception;
 
-import com.sofly.core.domain.workspace.code.WorkspaceErrorCode;
+import com.sofly.core.global.response.code.BaseErrorCode;
+
 import lombok.Getter;
 
 @Getter
 public class WorkspaceException extends RuntimeException {
 
-    private final WorkspaceErrorCode errorCode;
+    private final BaseErrorCode errorCode;
 
-    public WorkspaceException(WorkspaceErrorCode errorCode) {
+    public WorkspaceException(BaseErrorCode errorCode) {
         super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+    
+    public WorkspaceException(BaseErrorCode errorCode, String customMessage){
+        super(customMessage);
         this.errorCode = errorCode;
     }
 }
