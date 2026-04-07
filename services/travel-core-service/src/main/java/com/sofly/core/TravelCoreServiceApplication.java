8d9c3fd7a57c4bafb7e7c8e3ad33c69fc0448f69
Park Sang Min <sm0104224@gmail.com>
2026-04-07 10:13:21 +0900

    feat: TravelCoreServiceApplication.java edit -> plus @EnableScheduling for conquest map event

--- services/travel-core-service/src/main/java/com/sofly/core/TravelCoreServiceApplication.java
+++ services/travel-core-service/src/main/java/com/sofly/core/TravelCoreServiceApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableJpaAuditing 
+@EnableJpaAuditing
 @EnableFeignClients
+@EnableScheduling
 @ConfigurationPropertiesScan 
 public class TravelCoreServiceApplication {
 
