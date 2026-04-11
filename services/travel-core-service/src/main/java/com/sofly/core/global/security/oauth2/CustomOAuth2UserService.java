aa5e43910c290765d519bd167b7552409e02f957
Park Sang Min <sm0104224@gmail.com>
2026-04-12 01:11:32 +0900

    fix(oauth2): throw OAuth2Error with description for duplicate email

--- services/travel-core-service/src/main/java/com/sofly/core/global/security/oauth2/CustomOAuth2UserService.java
+++ services/travel-core-service/src/main/java/com/sofly/core/global/security/oauth2/CustomOAuth2UserService.java
@@ -1,19 +1,22 @@
 package com.sofly.core.global.security.oauth2;
 
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.OAuth2Error;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.sofly.core.domain.user.entity.User;
 import com.sofly.core.domain.user.repository.UserRepository;
 import com.sofly.core.global.security.oauth2.userinfo.GoogleUserInfo;
 import com.sofly.core.global.security.oauth2.userinfo.KakaoUserInfo;
 import com.sofly.core.global.security.oauth2.userinfo.NaverUserInfo;
 import com.sofly.core.global.security.oauth2.userinfo.OAuth2UserInfo;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
@@ -55,7 +58,9 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
         return userRepository.findByProviderAndProviderId(provider, userInfo.getProviderId())
                 .orElseGet(() -> {
                     if (userRepository.existsByEmail(userInfo.getEmail())) {
-                        throw new OAuth2AuthenticationException("이미 다른 소셜 계정으로 가입된 이메일입니다.");
+                        throw new OAuth2AuthenticationException(
+                            new OAuth2Error("duplicate_email", "이미 다른 소셜 계정으로 가입된 이메일입니다.", null)
+                        );
                     }
                     return userRepository.save(User.builder()
                             .email(userInfo.getEmail())
