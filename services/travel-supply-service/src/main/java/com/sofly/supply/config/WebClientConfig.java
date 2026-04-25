7a18a30f200fc886df7f223516b8ac06766b7bd9
Park Sang Min <sm0104224@gmail.com>
2026-04-26 01:11:33 +0900

    feat: supply zipkin tracing WebclientConfing bean injection

--- services/travel-supply-service/src/main/java/com/sofly/supply/config/WebClientConfig.java
+++ services/travel-supply-service/src/main/java/com/sofly/supply/config/WebClientConfig.java
@@ -14,12 +14,12 @@ public class WebClientConfig {
     private static final int BUFFER_SIZE = 10 * 1024 * 1024;
 
     @Bean("rapidApiWebClient")
-    public WebClient rapidApiWebClient(RapidApiProperties props){
+    public WebClient rapidApiWebClient(WebClient.Builder builder, RapidApiProperties props){
         ExchangeStrategies strategies = ExchangeStrategies.builder()
                 .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(BUFFER_SIZE))
                 .build();
 
-        return WebClient.builder()
+        return builder
                 .baseUrl(props.baseUrl())
                 .defaultHeader("X-RapidAPI-Key", props.apiKey())
                 .defaultHeader("X-RapidAPI-Host", props.host())
